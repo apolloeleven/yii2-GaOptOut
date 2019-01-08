750a189133f6802389028259bf7128f82b33c411
levani <levani19972@gmail.com>
2019-01-08 15:34:49 +0400

    Implement basic js implemetation

--- gaOpOut.php
+++ gaOpOut.php
@@ -11,14 +11,35 @@ namespace apollo11\yii2GaOptOut;
 
 class gaOpOut
 {
-//gap Id
+    // view
+    public $view;
+    //gap Id
     public $gaAppId;
-//output call event
+    //output call event
     public $optOutCallEvent = 'click';
-//element selector with link to jsoutput
+    //element selector with link to jsoutput
     public $elementSelector = '[href="#jsgaoptout"]';
-
-//Debug default false
+    //Debug default false
     public $debug = false;
 
+    public $showAlterAfterDeactivate = true;
+
+    public function registerGaOpOut($view, $gaAppId, $optOutCallEvent, $elementSelector, $debug, $showAlterAfterDeactivate)
+    {
+        Assets::register($view);
+        $view->registerJs("
+    <script>
+        (function() {
+            window.onload = function () {
+                var gaOptOut = new ApGaOptOut({
+                    gaAppId: $gaAppId,
+                    optOutCallEvent: $optOutCallEvent,
+                    elementSelector: $elementSelector,
+                    debug: $debug,
+                    showAlterAfterDeactivate: $showAlterAfterDeactivate
+                });
+            }
+        })();
+    </script>");
+    }
 }
